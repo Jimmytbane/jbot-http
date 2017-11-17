dbd469a4cf255a7412308e9bc7bc27a6c88043d2
JimmyBot <jmfgdev@users.noreply.github.com>
2017-11-17 18:39:06 -0500

    Create jbot-http.go

---
+++ jbot-http.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func response(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, it works!")
+}
+
+func main() {
+    http.HandleFunc("/", response)
+    http.ListenAndServe(":8080", nil)
+}
