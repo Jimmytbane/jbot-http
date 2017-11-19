fc4d97a6a54f4a9813241a0e722d0d14637a43de
JimmyBot <jmfgdev@users.noreply.github.com>
2017-11-19 04:42:01 +0000

    gofmt -s

--- jbot-http.go
+++ jbot-http.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func response(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, it works!")
+	fmt.Fprintf(w, "Hi there, it works!")
 }
 
 func main() {
-    http.HandleFunc("/", response)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", response)
+	http.ListenAndServe(":8080", nil)
 }
